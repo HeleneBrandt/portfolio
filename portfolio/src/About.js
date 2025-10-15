59cd234aca03a0e218fc2d9fbd9ea2b10692b175
HeleneBrandt <hebra8655@oslomet.no>
2025-10-15 13:12:16 +0200

    Initial commit

---
+++ portfolio/src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section id="about">
+            <h2>Om meg</h2>
+            <p>
+                Engasjert fullstack-utvikler med erfaring innen både frontend og backend.
+                Jeg elsker å bygge løsninger som kombinerer teknologi og brukervennlighet, og trives i tverrfaglige team.
+            </p>
+        </section>
+    );
+}
+
+export default About;
